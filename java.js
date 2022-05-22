478513c8edeb74d6bf9ccf01c0a3de9fcdc02b29
LENOVO <dytola1599@gmail.com>
2022-05-22 19:05:14 +0700

    java.js

---
+++ java.js
@@ -0,0 +1,70 @@
+package class_1;
+
+public class student {
+    public float java,html,php,system,sql;
+    public float total()
+    {
+           return java+html+php+system+sql;
+    }
+    public float average()
+    {
+        return this.total()/5;
+    
+    }
+     public student() {
+
+    }
+    public student(float java, float html, float php,float system,float sql) {
+        this.java = java;
+        this.html = html;
+        this.php = php;
+        this.system = system;
+        this.sql = sql;
+    }
+     public void showData() {
+        System.out.println("TOtal  :   " + total());
+        System.out.println("Average  : " + average());
+    }
+}
+public class student{
+    float widht,height;
+    public float area()
+    {
+        return widht*this.height;
+    }
+    public float parimater()
+    {
+        return (widht*height)/2;
+    }
+    public Rectangle{
+
+    }
+    public Rectangle(float widht,float height)
+    {
+        this.wight = widht;
+        this.height = height;
+    }
+    public  void showData()
+    {
+        System.out.print("Area :"+area());
+        System.out.print("Parimater :"+parimater());
+    }
+
+}
+    htmt 1test
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h2>
+                java test 
+            </h2>
+                <p>i like</p>
+                <p>do you like</p>
+                <p>id="deme"</p>
+                <script>
+                    class car{
+                        
+                    }
+                </script>
+        </body>
+    </html>
