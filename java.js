07b2d19270904941477590eac01b6beada1c19a2
DYTOLA <97717653+Dytola168@users.noreply.github.com>
2022-06-14 18:40:20 +0700

    Update java.js

---
+++ java.js
@@ -0,0 +1,12 @@
+#### JAVA
+###### TOTAL FORM IN JAVA
+~~~
+private void btSumActionPerformed(java.awt.event.ActionEvent evt) {                                      
+        float v1,v2,result;
+        v1=Float.parseFloat(textBox1.getText());
+        v2=Float.parseFloat(textBox2.getText());
+        result=v1+v2;
+        textResult.setText(result+"");
+    }                                     
+
+~~~
